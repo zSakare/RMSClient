e0ad17a97526869be894fa61b367dff764ac65eb
Albert Wang <albertwang2002@hotmail.com>
2015-05-21 14:51:49 +1000

    Added initial project bootstrapped

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+require('./router/main')(app);
+
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3000, function() {
+	console.log("We have started our server on port 3000");
+});
---
+++ views/template/footer.html
@@ -0,0 +1,2 @@
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
